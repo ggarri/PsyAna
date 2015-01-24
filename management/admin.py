f092ac206a45ba9f64fc49c3e621ca12aea37079
trivago-ggarrido <gabriel.garrido@trivago.com>
2015-01-24 12:57:15 +0100

    Updating admin panel and phone style

--- management/admin.py
+++ management/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from django.core.urlresolvers import reverse
 from management.models import UserProfile, Office
-# from tinymce.widgets import TinyMCE
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    exclude = ('user_permissions', 'last_login', 'date_joined', 'birthday')
-    # ('Profile description',  {'fields': ['description'], 'widget': TinyMCE})
+    exclude = ('user_permissions', 'last_login', 'date_joined', 'birthday', 'is_superuser')
+    # actions = ['make_something']
+
+    # def make_something(self, request, queryset):
+    #     if not request.user.is_superuser:
+    #         return queryset.update(request.user.id)
+
+    # def get_queryset(self):
+    #     if not self.request.user.is_superuser:
+    #         return super(self).get_queryset().filter(id=self.request.user.id)
+    #     else:
+    #         return super(self).get_queryset()
 
 
 class OfficeeAdmin(admin.ModelAdmin):
